2e895e6f4d212427c591f88a41b33eefc5ea26b9
Cristóbal <cristobalarau@hotmail.com>
2025-04-07 00:58:12 -0400

    Se terminan los modelos para products y store definiendo que hace cada uno de ellos junto a sus relaciones

--- app/Models/products.php
+++ app/Models/products.php
@@ -6,12 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class products extends Model
 {
-    
-    
-    protected $fillable = ['name', 'owner_id'];
-
     public function products() {
-        return $this->belongsToMany(products::class, 'store_products')
+        return $this->belongsToMany(Store::class, 'store_products')
                     ->withPivot('stock')
                     ->withTimestamps();
     
