fdda202484fb5dfa39c32c1005c0b2ed0005e113
Juan Orellana <orellanajuan812@gmail.com>
2025-04-13 16:21:40 -0400

    Se agrego el logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,13 +1,15 @@
-import AppLogoIcon from './app-logo-icon';
-
 export default function AppLogo() {
     return (
         <>
-            <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md border-radius-md border-2 border-sidebar-primary p-1">
+                <img
+                    src="/images/logoMype.png" // Ruta al archivo PNG
+                    alt="Logo Mi Mype"
+                    className="w-20 h-20 object-contain"
+                />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
+                <span className="mb-0.5 truncate leading-none font-semibold">Mi Mype</span>
             </div>
         </>
     );
